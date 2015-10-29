a9a8b131283de85a68a2bfb4767b9d2628d1b453
Bob Long <robertjflong@gmail.com>
2015-10-29 12:14:21 +0000

    bump

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ Plugin URI: https://wordpress.org/plugins/intercom
 Description: Official <a href="https://www.intercom.io">Intercom</a> support for Wordpress.
 Author: Bob Long
 Author URI: https://www.intercom.io
-Version: 2.2.1
+Version: 2.2.2
  */
 
 class SecureModeCalculator
