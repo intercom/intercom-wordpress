4006dce4051dbadec93696bd5946e2ab35f61af1
Skaelv <kevin.antoine@intercom.io>
2017-04-14 15:11:59 +0100

    bump to v2.5.13

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ Plugin URI: https://wordpress.org/plugins/intercom
 Description: Official <a href="https://www.intercom.io">Intercom</a> support for WordPress.
 Author: Intercom
 Author URI: https://www.intercom.io
-Version: 2.5.12
+Version: 2.5.13
  */
 
 class IdentityVerificationCalculator
