8fd4a72389eee2b0c4739b5fdc016324dd3b98e8
Kelly Miyashiro <kmiyashiro@users.noreply.github.com>
2017-11-03 12:23:15 -0400

    Bump version to 2.6

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ Plugin URI: https://wordpress.org/plugins/intercom
 Description: Official <a href="https://www.intercom.io">Intercom</a> support for WordPress.
 Author: Intercom
 Author URI: https://www.intercom.io
-Version: 2.5.16
+Version: 2.6.0
  */
 
 class IdentityVerificationCalculator
